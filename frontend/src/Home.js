c01d5b325b733cd314a4a28199f9ea6ef4a702d9
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-12-07 17:57:41 -0500

    Added styling potential for Home

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,20 +1,21 @@
 import React, {useContext} from "react";
 import { DataContext } from "./Context";
+import "./Home.css"
 
 function Home() {
     const {user} = useContext(DataContext)
 
     if(user.username){
         return (
-            <div>
+            <div className="home">
                 <h1>Shoply</h1>
-                <h2>Welcome {user.username}</h2>
+                <h2>Welcome "{user.username}"</h2>
             </div>
         )
     } //If user is currently logged in
 
     return (
-        <div>
+        <div className="home">
             <h1>Shoply</h1>
             <h2>Please register or login</h2>
         </div>
